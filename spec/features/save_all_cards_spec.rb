628e1caae97b3b4c5fe309eea37feb3339db21fb
patrickclery <patrick.clery@gmail.com>
2019-09-28 21:20:21 -0700

    :white_check_mark: Adding tests.

---
+++ spec/features/save_all_cards_spec.rb
@@ -0,0 +1,10 @@
+RSpec.describe 'Save all my recommendations into the database as raw data' do
+  context 'without delay' do
+    pending
+    fail
+  end
+  context 'using delayed background jobs' do
+    pending
+    fail
+  end
+end
