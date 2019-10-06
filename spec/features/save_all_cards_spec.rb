81e2624f49817f5ae7615552e6f04d51c0b29882
patrickclery <patrick.clery@gmail.com>
2019-10-05 17:53:52 -0700

    comments

--- spec/features/save_all_cards_spec.rb
+++ spec/features/save_all_cards_spec.rb
@@ -1,13 +1,9 @@
 RSpec.describe SaveRecommendations do
+  # Borrow the http request stubs from tinder_client gem.
   include_context 'http request stubs'
   subject { described_class }
 
   let(:api_token) { "eyJhbGciOiJIUzI1NiJ9.MTc3ODk5MDk4MDM.5q4R0H08rE0Dd9KgxMPp6jcTfIBLCXgEuVZfC9znJTE" }
-
-  # Returns all the results at once
-  before do
-    # Stub http requests
-  end
 
   it { should respond_to(:call).with_keywords(:api_token) }
 
