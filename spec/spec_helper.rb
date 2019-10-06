727418d966679c74918ae7c763b74e3ea4115580
patrickclery <patrick.clery@gmail.com>
2019-10-05 18:43:51 -0700

    this app is dumb to WebMock's presence (included in `tinder_client`)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -98,12 +98,6 @@ Rails.configuration do |c|
   end
 end
 
-
-############################################################
-# WEBMOCK
-require 'webmock'
-WebMock.enable!
-
 # Borrow the shared stubs from the tinder client
 gem_dir = Gem::Specification.find_by_name("tinder_client").gem_dir
 require "#{gem_dir}/spec/tinder/contexts/http_request_stubs"
