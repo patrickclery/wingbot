e4fb26576186f903953ae411dbaccb182ba7fd79
patrickclery <patrick.clery@gmail.com>
2019-10-06 23:48:56 -0700

    remove unnecessary requires

--- config/application.rb
+++ config/application.rb
@@ -19,10 +19,6 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-require 'tinder'
-require 'tinder/client'
-require 'tinder/get_recommended_users'
-
 module BetterTinder
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -37,3 +33,4 @@ module BetterTinder
     config.generators.system_tests = nil
   end
 end
+
