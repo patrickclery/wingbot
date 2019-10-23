62a3274c2d2ff5e62c521d8b22c85d85f9e07642
patrickclery <patrick.clery@gmail.com>
2019-10-23 01:04:28 -0700

    :recycle: Refactoring code. removed useless requires

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,10 +3,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 
 ### Support for CodeCov
-require 'simplecov'
 SimpleCov.start
-
-require 'codecov'
 SimpleCov.formatter = SimpleCov::Formatter::Codecov
 
 # Prevent database truncation if the environment is production
@@ -45,8 +42,6 @@ end
 
 ###############################################################################
 ### FactoryBot
-require 'factory_bot_rails'
-require 'factory_bot'
 
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
@@ -54,7 +49,6 @@ end
 
 ###############################################################################
 ### DatabaseCleaner
-require 'database_cleaner'
 RSpec.configure do |config|
 
   config.around(:each) do |example|
@@ -71,44 +65,9 @@ RSpec.configure do |config|
 
 end
 
-###############################################################################
-### Capybara
-# Check .env.test for examples of flags you can use in .env.test.local
-
-require "selenium/webdriver"
-# require "chromedriver-helper"
-
-Capybara.app_host              = ENV['HOST_URL']
-Capybara.current_driver        = ENV['CAPYBARA_DRIVER'].to_s.to_sym
-Capybara.default_max_wait_time = ENV['CAPYBARA_DEFAULT_MAX_WAIT'].to_s.to_sym
-
-# Helper that makes requests to the frontend skip rack and behave like normal requests
-def frontend
-  Capybara.app_host   = ENV['FRONTEND_URL']
-  Capybara.run_server = false
-  yield
-  Capybara.app_host   = nil
-  Capybara.run_server = true
-end
-
-# (Capybara) saves a screenshot and PNG of each test
-Rails.configuration do |c|
-  c.after(:each) do |example|
-    if example.metadata[:type] == :feature
-      save_and_open_page
-      save_and_open_screenshot
-    end
-  end
-end
-
 # Borrow the shared stubs from the tinder client
 gem_dir = Gem::Specification.find_by_name("tinder_client").gem_dir
 require "#{gem_dir}/spec/tinder/contexts/http_request_stubs"
 
-# Extra
-require 'faker'
-
-# GraphQL Spec
-require 'rspec/graphql_matchers'
 
 
