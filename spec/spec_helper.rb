deb613ad320d64feacf0c2eead67ae34b0f1eee3
patrickclery <patrick.clery@gmail.com>
2019-10-24 18:22:55 -0700

    :recycle: Refactoring code. :: was removed from Ruby recently, AFAIK

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,7 @@ require 'rspec/rails'
 ### General
 RSpec.configure do |config|
   config.order        = :random
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.fixture_path = "#{Rails.root}/spec/fixtures"
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
 
