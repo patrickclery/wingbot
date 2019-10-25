17a32f6ec454a3f94473d5c3a47af3f2f79fc7f0
patrickclery <patrick.clery@gmail.com>
2019-10-25 15:32:38 -0700

    :recycle: Refactoring code.

--- app/models/raw_data.rb
+++ app/models/raw_data.rb
@@ -1,2 +1,8 @@
+require 'tinder/client' # provides `Tinder::Updates`
+
 class RawData < ApplicationRecord
+  # @return Tinder::Updates A struct containing Tinder account data
+  def to_update
+    Tinder::Updates.new(data)
+  end
 end
