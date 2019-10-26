183e359751254fd7cedf3164cc9dfc9f712ba04d
patrickclery <patrick.clery@gmail.com>
2019-10-25 19:10:21 -0700

    :arrow_up: Upgrading dependencies.

--- Gemfile
+++ Gemfile
@@ -24,7 +24,7 @@ gem 'jbuilder', '~> 2.7'
 gem 'dry-initializer', git: 'https://github.com/dry-rb/dry-initializer.git'
 gem 'dry-struct', git: 'https://github.com/dry-rb/dry-struct.git'
 gem 'dry-types', git: 'https://github.com/dry-rb/dry-types.git'
-gem 'tinder_client', git: 'https://github.com/patrickclery/tinder_client.git'
+gem 'tinder_client', git: 'https://github.com/patrickclery/tinder_client.git', branch: 'master'
 gem 'graphql'
 
 group :test do
--- app/services/save_recommendations.rb
+++ app/services/save_recommendations.rb
@@ -10,7 +10,7 @@ class SaveRecommendations
     def call(api_token:) # Fetching recommendations...
       client           = Tinder::Client.new
       client.api_token = api_token
-      client.get_recommended_users.each do |card| # Then loop through each card within the collection
+      client.get_recommendations.each do |card| # Then loop through each card within the collection
         RawData.create(data: card.to_json, tag: 'recommended_users')
       end
       true
