5295e482f7fabb2a3ebd17c0438c1d555e3a8400
patrickclery <patrick.clery@gmail.com>
2019-10-28 05:56:54 -0700

    :recycle: Refactoring code.

--- db/migrate/20190930083654_create_raw_data.rb
+++ db/migrate/20190930083654_create_raw_data.rb
@@ -1,10 +1,9 @@
 class CreateRawData < ActiveRecord::Migration[6.0]
   def change
     create_table :raw_data do |t|
+      t.datetime :imported_at
       t.json :data
       t.string :tag
-      t.datetime :imported_at
-
       t.timestamps
     end
   end
