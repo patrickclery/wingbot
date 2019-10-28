c38083f749637158b80f17ea8d4e499c88612c54
patrickclery <patrick.clery@gmail.com>
2019-10-28 06:06:01 -0700

    :recycle: Refactoring code.

--- spec/services/process_raw_data_spec.rb
+++ spec/services/process_raw_data_spec.rb
@@ -1,12 +1,13 @@
-RSpec.describe ProcessRawData, type: :feature do
-
-  it { should respond_to(:call) }
+RSpec.describe 'ProcessRawData', type: :feature do
+  include_context 'default'
 
   it 'processes unimported raw data' do
+    pending
     expect { subject.call(api_token: api_token) }.to change { RawData.where(imported_at: nil).count }.from(3).to(0)
   end
 
   it 'returns true on success' do
+    pending
     expect(subject.call(api_token: api_token)).to be true
   end
 end
