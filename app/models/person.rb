1f80f1507fb03ecf30222fb1b26e88c9df8c3ca8
patrickclery <patrick.clery@gmail.com>
2019-10-29 04:09:37 -0700

    :recycle: Refactoring code.

--- app/models/person.rb
+++ app/models/person.rb
@@ -5,13 +5,12 @@ class Person < ApplicationRecord
   class << self
     # @param Tinder::User pass a User struct from a recommendation
     def from_user(user:)
-      # Mass assign everything
-      new birthdate: user.birth_date,
-          tinder_id: user._id,
+      new tinder_id: user._id,
           bio:       user.bio,
+          birthdate: user.birth_date,
+          gender:    user.gender,
           name:      user.name,
           photos:    user.photos,
-          gender:    user.gender,
           schools:   user.schools
     end
   end
