36ee4d45e087326b68c7e8f58b1eda32a09888b1
patrickclery <patrick.clery@gmail.com>
2019-10-30 02:04:09 -0700

    :recycle: Refactoring code.

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,17 +2,15 @@ class Person < ApplicationRecord
   has_many :matches
   serialize :photos
 
-  class << self
-    # @param Tinder::User pass a User struct from a recommendation
-    def from_user(user:)
-      new tinder_id: user._id,
-          bio:       user.bio,
-          birthdate: user.birth_date,
-          gender:    user.gender,
-          name:      user.name,
-          photos:    user.photos,
-          schools:   user.schools
-    end
+  # @param Tinder::User pass a User struct from a recommendation
+  def self.from_user(user:)
+    new bio:       user.bio,
+        birthdate: user.birth_date,
+        gender:    user.gender,
+        name:      user.name,
+        photos:    user.photos,
+        schools:   user.schools,
+        tinder_id: user._id
   end
 
 end
