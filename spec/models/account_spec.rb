8e9d47d978791b7bc2f28bf1cea535e228992952
patrickclery <patrick.clery@gmail.com>
2019-11-03 15:33:35 -0800

    :white_check_mark: Adding tests.

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -10,9 +10,12 @@ RSpec.describe Account, type: :model do
   it { should have_db_column(:last_active_at).of_type(:datetime) }
 
   describe '#from_profile' do
-    subject { described_class.from_profile(profile: profile) }
-    it { should be_a(described_class) }
-    it { expect { subject }.to change { Account.count }.by(1) }
+    subject { described_class.from_profile(profile) }
+    it { expect(subject).to be_a(described_class) }
+
+    it 'creates a new account from an ActiveProfile struct' do
+      expect { subject.save }.to change { Account.count }.by(1)
+    end
   end
 
 end
