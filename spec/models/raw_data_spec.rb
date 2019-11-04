e6bbdc2a5c01df67527d0a369bd11e735379b7ea
patrickclery <patrick.clery@gmail.com>
2019-11-03 16:40:54 -0800

    :recycle: Refactoring code. :white_check_mark: Adding tests.

--- spec/models/raw_data_spec.rb
+++ spec/models/raw_data_spec.rb
@@ -1,22 +1,23 @@
 RSpec.describe RawData, type: :model do
-  include Tinder
-  include_context 'raw data'
+  let(:raw_recommendations) { create(:raw_data_recommendations) }
+  let(:raw_updates) { create(:raw_data_updates) }
+  let(:raw_profile) { create(:raw_data_profile) }
 
-  it { should respond_to(:to_updates) }
   it { should respond_to(:to_recommendations) }
-
   context '#to_recommendations' do
-    subject { raw_data_recommendations.to_recommendations }
+    subject { raw_recommendations.to_recommendations }
     it { should be_an(Array) }
   end
 
+  it { should respond_to(:to_updates) }
   context '#to_updates' do
-    subject { create(:raw_data_updates).to_updates }
+    subject { raw_updates.to_updates }
     it { should be_an(Tinder::Updates) }
   end
 
+  it { should respond_to(:to_profile) }
   context '#to_profile' do
-    subject { create(:raw_data_profile).to_profile }
+    subject { raw_profile.to_profile }
     it { should be_a(Tinder::ActiveProfile) }
   end
 
