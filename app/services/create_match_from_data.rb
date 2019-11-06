b831b6c0c1125357a19c5a95f1d9e36052b319f9
patrickclery <patrick.clery@gmail.com>
2019-11-05 16:18:43 -0800

    :white_check_mark: Adding tests.

--- app/services/create_match_from_data.rb
+++ app/services/create_match_from_data.rb
@@ -2,7 +2,11 @@ class CreateMatchFromData
 
   # @return Account
   # @param Hash JSON response of from a profile request
-  def self.call(match:, person:, account:)
+  def self.call(match:, account:)
+    tinder_match_id = match._id
+    tinder_person_id = tinder_match_id.sub(account.tinder_id, '')
+    person = Person.find_by(tinder_id: tinder_person_id)
+
     obj = Match.new account:              account,
                     is_following:         match.following,
                     is_boost_match:       match.is_boost_match,
@@ -19,7 +23,7 @@ class CreateMatchFromData
                     person:               person,
                     readreceipt:          match.readreceipt,
                     seen:                 match.seen,
-                    tinder_match_id:      match._id
+                    tinder_match_id:      tinder_match_id
     obj.save!
     obj
   end
--- spec/services/create_match_from_data_spec.rb
+++ spec/services/create_match_from_data_spec.rb
@@ -1,20 +1,21 @@
 RSpec.describe CreateMatchFromData, type: :service do
   subject { described_class }
 
-  let!(:account) { create(:account) }
-  let!(:person) { create(:person, tinder_id: '89038190283xjfklsdjklfjs') }
+  let!(:account) { create(:account, tinder_id: 'AAAAAAAAAAA1111111111AAAA') }
+  let!(:person) { create(:person, tinder_id:   'BBBBBBBBBBB2222222222AAAA') }
   let!(:raw_updates) { create(:raw_data_updates) }
   let!(:updates) { raw_updates.to_updates }
   let!(:matches) { updates.matches }
+  # Stubs a match that associates account & person
   let!(:match) do
     obj = updates.matches.sample
-    allow(obj).to receive(:_id).and_return('89038190283xjfklsdjklfjs89038190283xjfklsdjklfjs')
+    allow(obj).to receive(:_id).and_return('AAAAAAAAAAA1111111111AAAABBBBBBBBBBB2222222222AAAA')
     obj
   end
 
-  it { should respond_to(:call).with_keywords(:match, :person, :account) }
+  it { should respond_to(:call).with_keywords(:match, :account) }
   describe '#call' do
-    subject(:valid_match) { described_class.call(match: match, person: person, account: account) }
+    subject(:valid_match) { described_class.call(match: match, account: account) }
     it { expect(subject).to be_a(Match) }
     it { expect { subject }.to change { Match.count }.by(1) }
   end
