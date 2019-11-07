fef9882098b722cd329b1c43e70a470059b90650
patrickclery <patrick.clery@gmail.com>
2019-11-06 16:21:38 -0800

    :recycle: Refactoring code. moved all to one file and refactored shared examples

--- spec/services/save_raw_data_spec.rb
+++ spec/services/save_raw_data_spec.rb
@@ -1,28 +1,37 @@
-RSpec.describe SaveRawData, type: :service do
+### All the Save* services use this same pattern of tests
+RSpec.shared_context 'save data' do
   include_context 'stubs'
 
-  describe '#call' do
-    subject { SaveRawData.call(api_token: api_token) }
-    let(:api_token) { "12a3bc45-a123-123a-1a23-1234abc4de5f" }
-    let!(:raw_profile) { create(:raw_data_profile) }
-    let!(:raw_recommendations) { create(:raw_data_recommendations) }
-    let!(:raw_updates) { create(:raw_data_updates) }
+  let(:api_token) { "eyJhbGciOiJIUzI1NiJ9.MTc3ODk5MDk4MDM.5q4R0H08rE0Dd9KgxMPp6jcTfIBLCXgEuVZfC9znJTE" }
+  let!(:raw_profile) { create(:raw_data_profile) }
+  let!(:raw_recommendations) { create(:raw_data_recommendations) }
+  let!(:raw_updates) { create(:raw_data_updates) }
 
-    it { expect(SaveRawData).to respond_to(:call).with_keywords(:api_token) }
-    it { expect { subject }.to change { RawData.where(imported_at: nil).count }.by(3) }
-    it { should be true }
+  subject { described_class.call(api_token: api_token) }
+  it { should be true }
+  it { expect(described_class).to respond_to(:call).with_keywords(:api_token) }
+  it { expect { subject }.to change { RawData.where(imported_at: nil) } }
+end
 
-    # https://relishapp.com/rspec/rspec-mocks/docs/setting-constraints/message-order
-    it 'imports data in logical order' do
-      allow(SaveProfile).to receive(:call).with(api_token: api_token).and_return(true)
-      allow(SaveRecommendations).to receive(:call).with(api_token: api_token).and_return(true)
-      allow(SaveUpdates).to receive(:call).with(api_token: api_token).and_return(true)
+###############################################################################
+RSpec.describe SaveRawData, type: :service do
+  include_examples 'save data'
 
-      expect(SaveProfile).to receive(:call).ordered
-      expect(SaveRecommendations).to receive(:call).ordered
-      expect(SaveUpdates).to receive(:call).ordered
+  # https://relishapp.com/rspec/rspec-mocks/docs/setting-constraints/message-order
+  it 'imports data in logical order' do
+    allow(SaveProfile).to receive(:call).with(api_token: api_token).and_return(true)
+    allow(SaveRecommendations).to receive(:call).with(api_token: api_token).and_return(true)
+    allow(SaveUpdates).to receive(:call).with(api_token: api_token).and_return(true)
 
-      subject
-    end
+    expect(SaveProfile).to receive(:call).ordered
+    expect(SaveRecommendations).to receive(:call).ordered
+    expect(SaveUpdates).to receive(:call).ordered
+
+    subject
   end
 end
+
+###############################################################################
+RSpec.describe(SaveProfile) { include_examples 'save data' }
+RSpec.describe(SaveRecommendations) { include_examples 'save data' }
+RSpec.describe(SaveUpdates) { include_examples 'save data' }
