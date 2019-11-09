f1873053ac4e8151276a1eed47459d81321037a2
patrickclery <patrick.clery@gmail.com>
2019-11-08 23:54:42 -0800

    :recycle: Refactoring code. Moved SaveRecommendations into its own because it needs to iterate responses

--- spec/services/save_account_data_spec.rb
+++ spec/services/save_account_data_spec.rb
@@ -17,14 +17,19 @@ RSpec.describe SaveRawData do
     it { expect(subject).to be true }
     it { expect { subject }.to change { RawData.where(imported_at: nil, tag: 'profile') } }
   end
-  context 'recommendations' do
-    subject { SaveRawData.call(tag: :recommendations, api_token: api_token) }
-    it { expect { subject }.to change { Account.count }.by(1) }
-    it { expect(subject).to be true }
-    it { expect { subject }.to change { RawData.where(imported_at: nil, tag: 'recommendations') } }
-  end
-
 end
+
+###############################################################################
+RSpec.describe SaveRecommendations do
+  include_context 'stubs'
+  let(:api_token) { "12a3bc45-a123-123a-1a23-1234abc4de5f" }
+
+  subject { SaveRecommendations.call(api_token: api_token) }
+  it { expect { subject }.to change { Account.count }.by(1) }
+  it { expect(subject).to be true }
+  it { expect { subject }.to change { RawData.where(imported_at: nil, tag: 'recommendations').count }.by(4) }
+end
+
 
 ###############################################################################
 RSpec.describe SaveAccountData, type: :service do
