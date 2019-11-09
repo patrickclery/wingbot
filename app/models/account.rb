98052d73997eb2a7349db30cd03d616e52018b9c
patrickclery <patrick.clery@gmail.com>
2019-11-08 16:44:41 -0800

    :recycle: Refactoring code.

--- app/models/account.rb
+++ app/models/account.rb
@@ -9,11 +9,11 @@ class Account < ApplicationRecord
   def self.from_profile(profile)
     find_or_initialize_by(tinder_id: profile.user._id).then do |account|
       account.assign_attributes is_email_verified: false,
-                            is_active:         false,
-                            name:              profile.user.name,
-                            email:             profile.account.account_email,
-                            phone_number:      profile.account.account_phone_number,
-                            data:              profile.to_json
+                                is_active:         false,
+                                name:              profile.user.name,
+                                email:             profile.account.account_email,
+                                phone_number:      profile.account.account_phone_number,
+                                data:              profile.to_json
       account
     end
   end
