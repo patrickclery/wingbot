3c1431c4751b33185dbe78c8ad7aa93e9d55369d
patrickclery <patrick.clery@gmail.com>
2019-11-08 17:57:53 -0800

    :recycle: Refactoring code.

--- app/graphql/graph_type/raw_data_type.rb
+++ app/graphql/graph_type/raw_data_type.rb
@@ -1,15 +1,16 @@
 module GraphType
   class RawDataType < BaseObject
+    include GraphQL::Types
 
-    field "id",         ID,                              null: false
-    field "json",       String,                          null: false
-    field "tag",        String,                          null: false
+    field "id",         ID,              null: false
+    field "json",       String,          null: false
+    field "tag",        String,          null: false
 
     # Timestamps
-    field "created-at", GraphQL::Types::ISO8601DateTime, null: false
-    field "updated-at", GraphQL::Types::ISO8601DateTime, null: false
-    field "deleted-at", GraphQL::Types::ISO8601DateTime, null: false
-    field "updated-at", GraphQL::Types::ISO8601DateTime, null: false
+    field "created-at", ISO8601DateTime, null: false
+    field "updated-at", ISO8601DateTime, null: false
+    field "deleted-at", ISO8601DateTime, null: false
+    field "updated-at", ISO8601DateTime, null: false
 
   end
 end
