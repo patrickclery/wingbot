662f1bd4461980d5f25b95937eda827230094498
patrickclery <patrick.clery@gmail.com>
2020-04-02 18:45:36 -0700

    :card_file_box: Performing database related changes. Removed migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_05_060000) do
+ActiveRecord::Schema.define(version: 2020_04_03_011745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -131,19 +131,6 @@ ActiveRecord::Schema.define(version: 2019_11_05_060000) do
     t.bigint "account_id"
     t.index ["account_id"], name: "index_raw_data_on_account_id"
   end
-
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
 
   add_foreign_key "matches", "accounts"
   add_foreign_key "matches", "people"
